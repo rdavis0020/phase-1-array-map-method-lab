d532398d5e9293dba304aef81542f1dbee8a83f7
Ryan Davis <rdavis0020@gmail.com>
2023-11-10 15:56:20 -0500

    Lab complete

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+    const titleCaseWords = words.map(word => word.charAt(0).toUpperCase() + word.slice(1));
+    return titleCaseWords.join(' ');
+  });
 }
+
+const titleCasedTutorials = titleCased();
+console.log(titleCasedTutorials);
